e32fb87b2c3fac60955b4aded6ca4d924b9100fd
yuxiang3 <yuxiang3@andrew.cmu.edu>
2020-07-26 01:16:08 -0700

    init

--- install.sh
+++ install.sh
@@ -29,4 +29,3 @@ ln -sf /root/.dotfiles/.tmux.conf.local /root/.tmux.conf.local
 ln -sf /root/.dotfiles/.vimrc           /root/.vimrc
 ln -sf /root/.dotfiles/.iterm2_shell_integration.zsh          /root/.iterm2_shell_integration.zsh
 
-source /root/.zshrc
